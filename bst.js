6a00a1c5c0ba7fdf2c01a143a1dccc434f2defcf
tonyg <aj.gianno@gmail.com>
2021-02-16 10:33:21 -0600

    insert

--- bst.js
+++ bst.js
@@ -15,4 +15,39 @@ class BinarySearchTree {
     this.left = null;
     this.right = null;
   }
+
+  insert(key, value) {
+    // if the tree is empty, then this key being inserted is the root of the tree
+    if ((this.key = null)) {
+      this.key = key;
+      this.value = value;
+    }
+    // if the tree already exists, then start at the root node, and compare it to the
+    // key you want to insert. If the new key is less than the node's key,
+    // then the new node needs to live in the left-hand branch.
+    else if (key < this.key) {
+      // if the existing node does not have a left child,
+      // meaning that if the `left` pointer is empty,
+      // then we can just instantiate and insert the new node as the left child of that node.
+      // passing `this` as the parent.
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      } else {
+        /* If the node has an existing left child, then we recursively call the `insert`
+    method so the node is added further down the tree  */
+        this.left.insert(key, value);
+      }
+    } else {
+      /* Similarly, if the new key is greater than the node's key
+  then you do the same thing, but on the right-hand side */
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      } else {
+        this.right.insert(key, value);
+      }
+      // because each row in a balanced tree contains 2 times as many nodes as the node before,
+      // the width grows exponentially with the number of nodes.Conversely,
+      // the height must grow logarithmically with the number of nodes. O(log(n)).
+    }
+  }
 }
