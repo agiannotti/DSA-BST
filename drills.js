972d28bbfda61c6aba867785aeb0ac04d8c5d219
tonyg <aj.gianno@gmail.com>
2021-02-17 21:40:29 -0600

    oops

--- drills.js
+++ drills.js
@@ -1,9 +1,9 @@
 class BinarySearchTree {
   constructor(key = null, value = null, parent = null) {
     this.key = key;
-    this.parent = parent;
     this.value = value;
     this.right = null;
     this.left = null;
+    this.parent = parent;
   }
 }
